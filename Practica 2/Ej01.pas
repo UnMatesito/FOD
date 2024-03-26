c098bcd49fd96d1aa539f7edd6e96cbeff1dd0d0
Mateo Suarez <mateosuarez1905@hotmail.com>
2024-03-26 13:02:10 -0300

    Update Exercises 1 & 2

--- Practica 2/Ej01.pas
+++ Practica 2/Ej01.pas
@@ -9,23 +9,20 @@ type
 
     archivo_empleados = file of empleado;
 
+procedure compactarArchivo(var arch: archivo_empleados; var arch_nuevo: archivo_empleados);
 var
-    arch: archivo_empleados;
-    arch_nuevo: archivo_empleados;
-    regA: empleado;
-    codActual: integer;
     montoTotal: real;
+    codActual: integer;
+    regA: empleado;
 begin
-    assign(arch, 'Archivo_antiguo');
-    assign(arch_nuevo, 'Archivo_Compactado');
-    reset(arch);
+    reset(arch); //se dispone de empleados
     rewrite(arch_nuevo);
     while (not eof(arch)) do
         begin
             montoTotal := 0;
             read(arch, regA);
             codActual := regA.codigo;
-            while (regA.codigo = codActual) do
+            while (not eof(arch) and (regA.codigo = codActual) do
                 begin
                     montoTotal := montoTotal + regA.monto;
                     read(arch, regA);
@@ -35,4 +32,13 @@ begin
         end;
     close(arch);
     close(arch_nuevo);
+end;
+
+var
+    arch: archivo_empleados;
+    arch_nuevo: archivo_empleados;
+begin
+    assign(arch, 'Archivo_Comisiones');
+    assign(arch_nuevo, 'Archivo_Compactado');
+    compactarArchivo(arch, arch_nuevo);
 end.
--- Practica 2/Ej02.pas
+++ Practica 2/Ej02.pas
@@ -1,8 +1,10 @@
 program E02P2
 const
-    s_aprobacion = 1..2;
+    valorAlto = 9999;
 
 type
+    s_aprobacion = 1..2; // 1: cursada | 2: final
+    
     alumno = record
         codigo: integer;
         apellido: string;
@@ -20,8 +22,87 @@ type
 
     archivo_detalle = file of detalle;
 
-var
-    
+procedure leerDetalle(det: archivo_detalle, dato: detalle);
 begin
-    assign() //se dispone de alumnos
+    if (not eof(det)) then
+        read(det, dato)
+    else
+        dato.codigo := valorAlto;
+end;
+
+procedure actMaestro(var mae: archivo_alumnos);
+var
+    det: archivo_detalle;
+    regM: alumno;
+    regD: detalle;
+    cantCursadas: integer;
+    cantFinales: integer;
+    aux: integer;
+begin
+    assign(det, 'Detalle_Alumnos');
+    reset(mae); reset(det);
+    read(mae, regM);
+    leer(det, regD);
+    while (regD <> valorAlto) do
+        begin
+            aux = regD.codigo;
+            cantCursadas := 0; cantFinales := 0;
+            while (aux = regD.codigo) do
+                begin
+                    if (regD.queAprobo = 1) then
+                        cantCursadas := cantCursadas + 1
+                    else
+                        cantFinales := cantFinales + 1;
+                    leer(det, regD);
+                end;
+            while (aux <> regD.codigo) do
+                read(mae, regM);
+            regM.cursadas := regM.cursadas + cantCursadas;
+            regM.aprobadas := regM.aprobadas + cantFinales;
+            seek(mae, filepos(mae)-1);
+            write(mae, regM);
+            if (not eof(mae)) then
+                read(mae, regM);
+        end;
+    close(det);
+    close(mae);
+end;
+
+procedure crearTxt(var mae: archivo_alumnos);
+var
+    arch_finales: Text;
+    a: alumno;
+begin
+    assign(arch_finales, 'AlumnosFinales.txt');
+    reset(mae);
+    rewrite(arch_finales);
+    while (not eof(mae)) do
+        begin
+            read(mae, a);
+            if (a.aprobadas > a.cursadas) then
+                writeln(arch_finales, 'Codigo de alumno: ', a.codigo,' Apellido: ', a.apellido, ' Nombre: ', a.nombre, ' Materias con cursada aprobada: ', a.cursada, ' Materias con final aprobado: ', a.final);
+        end;
+    close(mae);
+    close(arch_finales);
+end;
+
+var
+    opcion: char;
+    a: archivo_alumnos;
+begin
+    assign(a, 'Archivo_Alumnos');
+    writeln('-Ejercicio 2--------------------------------------------------');
+    writeln();
+    writeln('Ingrese una opcion');
+    writeln();
+    writeln('A: Actualizar el archivo de alumnos con un detalle');
+    writeln('B: Listar en un archivo de texto alumnos con mas finales aprobados');
+    writeln();
+    write('Opcion')readln(opcion);
+    opcion := UpCase(opcion);
+    case opcion of
+        'A': actMaestro(a);
+        'B': crearTxt(a);
+        else
+            writeln('La opcion no existe');
 end.
